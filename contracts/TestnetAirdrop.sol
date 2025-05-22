aaa7439a3976180104c62f316bfcedb966d21308
GeniDexDev <genidexdev@genidex.org>
2025-05-22 17:41:30 +0700

    feat: add withdrawRemainingTokens to TestnetAirdrop contract

--- contracts/TestnetAirdrop.sol
+++ contracts/TestnetAirdrop.sol
@@ -1,47 +1,46 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestnetAirdrop {
+contract TestnetAirdrop is Ownable {
     bytes32 public rewardRoot;
-    bytes32 public referralRoot;
     mapping(address => bool) public hasClaimed;
+    IERC20 public geniToken;
 
     event Claimed(address indexed user, uint256 amount);
 
-    constructor(bytes32 _rewardRoot, bytes32 _referralRoot) {
+    constructor(address _geniToken, bytes32 _rewardRoot) Ownable(msg.sender){
+        geniToken = IERC20(_geniToken);
         rewardRoot = _rewardRoot;
-        referralRoot = _referralRoot;
     }
 
     function claim(
         uint256 amount,
-        bytes32[] calldata proof
+        bytes32[] calldata proof,
+        bytes32 salt
     ) external {
         require(!hasClaimed[msg.sender], "Already claimed");
 
         bytes32 leaf = keccak256(
-            bytes.concat(keccak256(abi.encode(msg.sender, amount)))
+            bytes.concat(keccak256(abi.encode(msg.sender, amount, salt)))
         );
         require(MerkleProof.verify(proof, rewardRoot, leaf), "Invalid proof");
+        require(geniToken.balanceOf(address(this)) >= amount, "Not enough tokens");
 
         hasClaimed[msg.sender] = true;
-        // ERC20(token).transfer(msg.sender, amount);
+        geniToken.transfer(msg.sender, amount);
 
         emit Claimed(msg.sender, amount);
     }
 
-    function verifyReferral(
-        bytes32[] calldata proof,
-        address referrer,
-        address[] calldata referees
-    ) public view returns (bool) {
-        bytes32 leaf = keccak256(
-            bytes.concat(keccak256(abi.encode(referrer, referees)))
-        );
-        require(MerkleProof.verify(proof, referralRoot, leaf), "Invalid proof");
-        return true;
+    function withdrawRemainingTokens() external onlyOwner {
+        uint256 balance = geniToken.balanceOf(address(this));
+        require(balance > 0, "No tokens to withdraw");
+        require(geniToken.transfer(owner(), balance), "Withdraw failed");
     }
+
 }
